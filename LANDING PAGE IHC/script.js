25a79bf479e62c008e3655da6c0a4aaff7bfa40e
JuanqQuijano <118690618+JuanqQuijano@users.noreply.github.com>
2024-10-30 20:55:00 -0500

    Add files via upload

---
+++ LANDING PAGE IHC/script.js
@@ -0,0 +1,38 @@
+function enviarMensaje() {
+    // 
+    const mensajeInput = document.getElementById('mensaje-input');
+    const mensajeTexto = mensajeInput.value.trim();
+
+    // Si el texto está vacio, no enviará nada
+    if (mensajeTexto !== '') {
+        // Crear un nuevo contenedor para el mensaje propio
+        const nuevoMensaje = document.createElement('div');
+        nuevoMensaje.classList.add('mensaje-propio');
+
+        // Crear el párrafo que contendrá el texto del mensaje
+        const mensajeParrafo = document.createElement('p');
+        mensajeParrafo.textContent = mensajeTexto;
+
+        // Añadir el párrafo al contenedor del nuevo mensaje
+        nuevoMensaje.appendChild(mensajeParrafo);
+
+     
+        const mensajesContainer = document.getElementById('mensajes');
+        
+        if (!mensajesContainer) {
+            console.error("Contenedor de mensajes no encontrado");
+            return;
+        }
+
+        mensajesContainer.appendChild(nuevoMensaje);
+        console.log("Mensaje enviado:", mensajeTexto);
+
+      
+        mensajeInput.value = '';
+
+        // Desplazar al mandar un mensaje
+        mensajesContainer.scrollTop = mensajesContainer.scrollHeight;
+    } else {
+        console.log("El campo de mensaje está vacío.");
+    }
+}
